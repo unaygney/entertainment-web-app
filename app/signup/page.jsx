7e431c49e7abe58cdfb2bb8df48216fd692634e0
Guney Unay <guneymarketing17@hotmail.com>
2023-12-09 20:40:49 +0300

    fixed some minor bugs

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,36 +1,32 @@
 import React from "react";
+import LogoImg from "@/assets/icons/logo.svg";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
 import Link from "next/link";
-import LogoImg from "@/assets/icons/logo.svg";
-export const metadata = {
-  title: "Login Page",
-  description: "Entertainment Web App Login Page ",
-};
-
-function Login() {
+function Signup() {
   return (
     <main className="w-screen h-screen bg-[#10141E] px-6 py-12 flex flex-col gap-10 items-center justify-center">
       <LogoImg />
       <div className="w-full max-w-[400px] p-8  bg-[#161D2F] flex flex-col gap-10 rounded-[20px]">
         <h1 className="text-white text-4xl font-light tracking-[0.5px] leading-normal ">
-          Login
+          Sign Up
         </h1>
         <form className="w-full flex flex-col gap-4 ">
           <Input text="email" placeholder="Email address" />
           <Input text="password" placeholder="Password" />
-          <Button type={"submit"} title={"Login to your account"} />
+          <Input text="password" placeholder="Repeat password" />
+          <Button type={"submit"} title={"Create an account"} />
         </form>
         <Link
           className="text-[15px] font-light leading-normal text-white text-center "
-          href={"/signup"}
+          href={"/login"}
         >
-          Dont have an account?
-          <span className="text-[#FC4747] ml-2 ">Sign Up</span>
+          Already have an account?
+          <span className="text-[#FC4747] ml-2 ">Login</span>
         </Link>
       </div>
     </main>
   );
 }
 
-export default Login;
+export default Signup;
