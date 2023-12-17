90be3a33ac2776a7a4962246b890f6437319f812
Guney Unay <guneymarketing17@hotmail.com>
2023-12-17 20:10:22 +0300

    final touch

--- containers/BookmarkContainer/index.jsx
+++ containers/BookmarkContainer/index.jsx
@@ -2,9 +2,8 @@
 import Navbar from "@/components/Navbar";
 import Input from "@/components/Input";
 import React, { useState } from "react";
-import TrendMovies from "@/components/TrendMovies";
 import SearchIcon from "@/assets/icons/icon-search.svg";
-import AllMovies from "@/components/AllMovies";
+
 import Card from "@/components/Card";
 
 function BookmarkContainer({ bookmarked }) {
@@ -27,7 +26,7 @@ function BookmarkContainer({ bookmarked }) {
   return (
     <div className="w-full min-h-screen  bg-[#10141E] pb-10   ">
       <div className="flex flex-col lg:flex-row md:p-6 lg:p-8 ">
-        <Navbar activePage={"dashboard"} />
+        <Navbar activePage={"bookmark"} />
         {search ? (
           <main className="overflow-hidden w-full h-full px-6">
             <div className="flex items-center px-4 mt-6">
